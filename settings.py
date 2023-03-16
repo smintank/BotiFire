9361fcec39c3404a97d1a22b150f94017dfe608f
smintank <assbestmail@gmail.com>
2023-03-16 23:20:18 +0300

    Добавлен словарь дефолтных сообщений MESSAGE значения которого берутся из модуля defaults.py

--- settings.py
+++ settings.py
@@ -1,5 +1,14 @@
+import defaults as df
+
 DB_NAME = 'shifts.db'
 DB_PATH = 'db'
 
 
 DEL_DB = True
+
+MESSAGE = {
+    'start': df.START_MESSAGE,
+    'help': df.HELP_MESSAGE,
+    'name_err': df.NAME_CHECK_ERROR_MESSAGE,
+    'surname_err': df.SURNAME_CHECK_ERROR_MESSAGE
+}
